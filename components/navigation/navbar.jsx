f8a0a27242ca78222d1efbc347412862161de39e
mbothra <muditbothra22@gmail.com>
2023-10-02 09:12:17 +0200

    Full screen

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+			<a target={"_blank"}>
 				<img
 					className={styles.alchemy_logo}
 					src="/Chainlink.png"
